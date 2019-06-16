752361b2339cd08afecc5aba6a9adbc1b055d934
htu <hongjian.tu@dxc.com>
2019-06-16 09:40:43 +0800

    新規作成

---
+++ c2sfw-batch/src/main/java/jp/co/tokyo_gas/batch/c2sfwbatch/C2sfwBatchApplication.java
@@ -0,0 +1,13 @@
+package jp.co.tokyo_gas.batch.c2sfwbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2sfwBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C2sfwBatchApplication.class, args);
+	}
+
+}
---
+++ c2sfw-batch/src/test/java/jp/co/tokyo_gas/batch/c2sfwbatch/C2sfwBatchApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.co.tokyo_gas.batch.c2sfwbatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class C2sfwBatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
